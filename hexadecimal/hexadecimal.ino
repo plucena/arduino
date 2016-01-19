b4cc100ee8b56cc52000b2e42bc7cdf1c4add054
percival@gmail.com <plucena@gmail.com>
2016-01-19 21:07:12 -0200

    fixes

--- hexadecimal/hexadecimal.ino
+++ hexadecimal/hexadecimal.ino
@@ -11,25 +11,31 @@ void setup() {
 
 // the loop function runs over and over again forever
 void loop() {
-  int i=0;
-     uint32_t example[5] = {0x48, 0x45, 0x4c, 0x4c, 0x4f};
-     char stringBuffer[30]; // Needs to be more than big enough to hold all the digits of an int
-     char outputStrings[6];
+     uint32_t entrada[5] = {0x48, 0x45, 0x4c, 0x4c, 0x4f};
+     char *saida = convert(entrada, 5);
+  
+    // as per comment from LS_dev, platform is int 16bits
+    Serial.begin(9600);
+    Serial.println(saida);
+   
+    //digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
+    //delay(1000);              // wait for a second
+    //digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
+    delay(1000);              // wait for a second
+}
 
-  for (i = 0 ; i < 5 ; i++)
+
+char* convert(uint32_t example[], int size) {
+  int i=0;
+  char stringBuffer[size*4]; // Needs to be more than big enough to hold all the digits of an int
+  char *outputStrings = (char *) malloc(sizeof(char) * (size+1));
+
+  for (i = 0 ; i < size ; i++)
   {
-    snprintf(stringBuffer, 20, "%c", example[i]);
+    snprintf(stringBuffer, (size*4), "%c", example[i]);
     outputStrings[i] = *stringBuffer;
   }
-  outputStrings[5]=0;
-
-  
-   // as per comment from LS_dev, platform is int 16bits
-   Serial.begin(9600);
-    Serial.println(outputStrings);
-   
-  digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);              // wait for a second
-  digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);              // wait for a second
+  outputStrings[(size+1)]=0;
+  return outputStrings;
 }
+
