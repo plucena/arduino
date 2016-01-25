190948be56131f88c39a943f8aff906be83d7fc7
percival@gmail.com <plucena@gmail.com>
2016-01-25 16:23:04 -0200

    LCD

--- hexadecimal/hexadecimal.ino
+++ hexadecimal/hexadecimal.ino
@@ -12,17 +12,37 @@ void setup() {
 // the loop function runs over and over again forever
 void loop() {
      uint32_t entrada[5] = {0x48, 0x45, 0x4c, 0x4c, 0x4f};
-     char *saida = convert(entrada, 5);
+     // converte hexadecimais em String
+     char *saida =  convert(entrada, 5);
   
     // as per comment from LS_dev, platform is int 16bits
     Serial.begin(9600);
     Serial.println(saida);
-   
+    
+     // vamos quebrar a String saida 
+     char *pos = saida;
+     // declara as variaveis que eu quero quebrar
+     char string1[4] = {0}; // 3 caractes
+     char string2[3] = {0}; // 2 caracteres 
+     // ... declara outra strings...
+
+     // copia primeira variavel String
+     strncpy ( string1, pos, 3 );
+     // atualiza posicao
+     pos+=3;
+     // copia 2a string
+     strncpy ( string2, pos, 2 );
+     *pos+=2;
+      
+     Serial.println(string1);
+     Serial.println(string2);
+
     //digitalWrite(13, HIGH);   // turn the LED on (HIGH is the voltage level)
     //delay(1000);              // wait for a second
     //digitalWrite(13, LOW);    // turn the LED off by making the voltage LOW
     delay(1000);              // wait for a second
 }
+
 
 
 char* convert(uint32_t example[], int size) {
@@ -35,7 +55,7 @@ char* convert(uint32_t example[], int size) {
     snprintf(stringBuffer, (size*4), "%c", example[i]);
     outputStrings[i] = *stringBuffer;
   }
-  outputStrings[(size+1)]=0;
+  outputStrings[(size)]=0;
   return outputStrings;
 }
 
